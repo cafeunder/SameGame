3d622c73039f7f7bb6458aa762566d9dbb5193f0
cafeunder <cafe3641@gmail.com>
2015-12-16 15:49:51 +0900

    ブロックの削除＆詰め処理実装

--- SameGame/src/game/Map.java
+++ SameGame/src/game/Map.java
@@ -40,6 +40,7 @@ public class Map {
 	private int yNum;
 	private int offSetX;
 	private int offSetY;
+	private int columnNum;
 	private Point mousePoint;
 	private int[][] map;
 	private final int tipSize;
@@ -51,11 +52,14 @@ public class Map {
 		this.yNum = MAP_HEIGHT/tipSize;
 		this.offSetX = offSetX;
 		this.offSetY = offSetY;
-		this.map = new int[yNum][xNum];
 		
-		for(int y = 0; y < yNum; y++){
-			for(int x = 0; x < xNum; x++){
-				this.map[y][x] = Controller.getRand(typeNum);
+		// row X column
+		// due to SAMEGAME's board
+		this.map = new int[xNum][yNum];
+		
+		for(int x = 0; x < xNum; x++){
+			for(int y = 0; y < yNum; y++){
+				this.map[x][y] = Controller.getRand(typeNum);
 			}
 		}
 		
@@ -80,15 +84,84 @@ public class Map {
 			this.mousePoint = null;
 			this.selectPointGroup = null;
 		}
+		
+		if(mf.getMouseLeftPressCount() == 1 && this.selectPointGroup != null){
+			for(Point p : this.selectPointGroup){
+				this.map[p.x][p.y] = -1;
+			}
+			
+			//Column Check
+			for(int x = 0; x < this.xNum; x++){
+				for(int y = 0; y < this.yNum; y++){
+					int index = this.yNum-1 - y;
+					if(this.map[x][index] == -1){
+						int searchIndex = index-1;
+						while(true){							
+							if(searchIndex < 0) break;
+							
+							if(this.map[x][searchIndex] != -1){
+								this.map[x][index] = this.map[x][searchIndex];
+								this.map[x][searchIndex] = -1;
+								break;
+							}
+							
+							searchIndex--;
+						}
+
+						if(searchIndex < 0) break;
+					}
+				}
+			}
+			
+			//Row Check
+			for(int x = 0; x < this.xNum-1; x++){
+				boolean exist = false;
+				for(int y = 0; y < this.yNum; y++){
+					if(this.map[x][y] != -1){
+						exist = true;
+						break;
+					}
+				}
+				if(!exist){
+					this.map[x] = null;
+				}				
+			}
+			
+			for(int x = 0; x < this.xNum-1; x++){
+				if(this.map[x] == null){
+					int searchIndex = x+1;
+					while(true){							
+						if(searchIndex >= this.xNum) break;
+						
+						if(this.map[searchIndex] != null){
+							this.map[x] = this.map[searchIndex];
+							this.map[searchIndex] = null;
+							break;
+						}
+						
+						searchIndex++;
+					}				
+				}
+			}
+			
+			int prevXNum = this.xNum;
+			for(int x = 0; x < prevXNum; x++){
+				if(this.map[x] == null) break;
+				this.xNum = x;
+			}
+			this.xNum++;
+		}
 	}
 	
 	private Color[] DEBUG_COL = {new Color(255,0,0), new Color(0,255,0), new Color(0,0,255), new Color(255,200,0)};
 	public void draw(){
 		DrawLibrary dLib = DrawLibrary.getInstance();
 		
-		for(int y = 0; y < this.yNum; y++){
-			for(int x = 0; x < this.xNum; x++){
-				dLib.drawRect(this.offSetX + x*this.tipSize, this.offSetY + y*this.tipSize, this.tipSize, this.tipSize, DEBUG_COL[this.map[y][x]], true);
+		for(int x = 0; x < this.xNum; x++){
+			for(int y = 0; y < this.yNum; y++){
+				if(this.map[x][y] != -1){
+					dLib.drawRect(this.offSetX + x*this.tipSize, this.offSetY + y*this.tipSize, this.tipSize, this.tipSize, DEBUG_COL[this.map[x][y]], true);
+				}
 			}
 		}
 		if(this.mouseEntered()){
@@ -103,41 +176,43 @@ public class Map {
 	}
 
 	public Point[] calcNeighbor(Point op){
-		final int type = this.map[op.y][op.x];
+		if(op.x < 0 || op.x >= this.xNum || op.y < 0 || op.y >= this.yNum) return null;
+		
+		final int type = this.map[op.x][op.y];
 		if(type == -1){
 			return null;
 		}
 		
 		ArrayList<Point> result = new ArrayList<Point>(0); 
 		Queue<Point> queue = new ArrayDeque<Point>(0);
-		int[][] tempMap = new int[this.yNum][this.xNum];
+		int[][] tempMap = new int[this.xNum][this.yNum];
 		queue.add(op.copy());
-		tempMap[op.y][op.x] = 1;
+		tempMap[op.x][op.y] = 1;
 		result.add(op.copy());
 		
 		while(true){
 			Point p = queue.poll();
 			if(p == null) break;
 			
-			if(p.x-1 >= 0 && tempMap[p.y][p.x-1] == 0 && this.map[p.y][p.x-1] == type){
+			if(p.x-1 >= 0 && tempMap[p.x-1][p.y] == 0 && this.map[p.x-1][p.y] == type){
 				result.add(new Point(p.x-1, p.y));
 				queue.add(new Point(p.x-1, p.y));
-				tempMap[p.y][p.x-1] = 1;
+				tempMap[p.x-1][p.y] = 1;
 			}
-			if(p.x+1 < this.xNum && tempMap[p.y][p.x+1] == 0 && this.map[p.y][p.x+1] == type){
+			if(p.x+1 < this.xNum && tempMap[p.x+1][p.y] == 0 && this.map[p.x+1][p.y] == type){
 				result.add(new Point(p.x+1, p.y));
 				queue.add(new Point(p.x+1, p.y));
-				tempMap[p.y][p.x+1] = 1;
+				tempMap[p.x+1][p.y] = 1;
 			}
-			if(p.y-1 >= 0 && tempMap[p.y-1][p.x] == 0 && this.map[p.y-1][p.x] == type){
+			if(p.y-1 >= 0 && tempMap[p.x][p.y-1] == 0 && this.map[p.x][p.y-1] == type){
 				result.add(new Point(p.x, p.y-1));
 				queue.add(new Point(p.x, p.y-1));
-				tempMap[p.y-1][p.x] = 1;
+				tempMap[p.x][p.y-1] = 1;
 			}
-			if(p.y+1 < this.yNum && tempMap[p.y+1][p.x] == 0 && this.map[p.y+1][p.x] == type){
+			if(p.y+1 < this.yNum && tempMap[p.x][p.y+1] == 0 && this.map[p.x][p.y+1] == type){
 				result.add(new Point(p.x, p.y+1));
 				queue.add(new Point(p.x, p.y+1));
-				tempMap[p.y+1][p.x] = 1;
+				tempMap[p.x][p.y+1] = 1;
 			}			
 		}
 		
